7994a859cc065b29f66ddb7c60334769c64af691
Waseem Ahmad Naeem <ahmadwaseem237@gmail.com>
2017-06-27 18:33:07 +0500

    SelectedArea Class Completed

---
+++ Streamship Screenshot Tool/Business Logic/SelectedArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic
+{
+    class SelectedArea
+    {
+        public PointF coordX { get; set; }
+        public PointF coordY { get; set; }
+    }
+}
